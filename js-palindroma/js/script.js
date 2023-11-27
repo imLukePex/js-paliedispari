d7d973ccf471b235af093d4cabc0f7979cc93b16
imLukePex <luca.pessione@gmail.com>
2023-11-27 16:38:13 +0100

    Struttura ciclo while + funzione

--- js-palindroma/js/script.js
+++ js-palindroma/js/script.js
@@ -1,30 +1,63 @@
 // Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma.
 
-let parola = "";
 
 // Chiediamo a User una parola
 const userParola = prompt("Inserisci una parola");
 console.log(userParola);
 
-// Creaiamo variabile singolo carattere e variabile userParola invertita
-let carattere;
-let parolaInversa = "";
-
-// Settiamo valore dell'indice
-let i = userParola.length - 1;
-
-// Ciclo while per scorrere ogni carattere della parola dalla fine all'inizio
-while (i >= 0) {
-    carattere = userParola[i];
-    parolaInversa += carattere;
-    console.log(carattere);
-    i--;
-}
-
-console.log(parolaInversa);
-
-if(parolaInversa === userParola) {
+// Condizione da verificare per capire se la parola inserita sia palindroma o no
+if(parolaInversa === typeParola(userParola)) {
     console.log("La parola inserita è palindroma");
 } else {
     console.log("La parola inserita non è palindroma");
 }
+
+// Funzione che capisce se la parola inserita è palindroma o no
+function typeParola(parolaToCheck) {
+    
+
+    // Check se parola è palindroma o no
+    let parolaInversa = "";
+
+    // Settiamo valore dell'indice
+    let i = parolaToCheck.length - 1;
+
+    // Ciclo while per scorrere ogni carattere della parola dalla fine all'inizio
+    while (i >= 0) {
+        parolaInversa += parolaToCheck[i];
+        console.log(parolaToCheck);
+        i--;
+    }
+
+    return parolaInversa;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Creaiamo variabile singolo carattere e variabile userParola invertita
+// let carattere;
+// let parolaInversa = "";
+
+// // Settiamo valore dell'indice
+// let i = userParola.length - 1;
+
+// // Ciclo while per scorrere ogni carattere della parola dalla fine all'inizio
+// while (i >= 0) {
+//     carattere = userParola[i];
+//     parolaInversa += carattere;
+//     console.log(carattere);
+//     i--;
+// }
+
+// console.log(parolaInversa);
